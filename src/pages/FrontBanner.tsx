5fdb71a82144b1ef1a8a91ea0a4bc81b2ff47f4d
Shaunak Badani <shaunak.badani@duke.edu>
2026-03-27 01:45:52 -0400

    Finished adding header part 1 and initial files

---
+++ src/pages/FrontBanner.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './FrontBanner.module.css';
+
+const FrontBanner = () => {
+    return (
+        <header className={styles.header}>
+            Some Text...
+        </header> 
+    );
+};
+
+export default FrontBanner;
