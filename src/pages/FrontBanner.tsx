f8d2bb6fc4dad7e983a8568a32243a805af50446
Shaunak Badani <shaunak.badani@duke.edu>
2026-03-27 02:28:05 -0400

    Header part 2 done

--- src/pages/FrontBanner.tsx
+++ src/pages/FrontBanner.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import styles from './FrontBanner.module.css';
+import logo from "../assets/img/logo-white.png";
 
 const FrontBanner = () => {
     return (
         <header className={styles.header}>
-            Some Text...
+            <div className={styles.logoBox}>
+                <img src={logo} alt="Logo" className={styles.logo} />
+            </div>
+            <div className={styles.textBox}>
+                <h1 className={styles.headingPrimary}>
+                    <span className={styles.headingPrimaryMain}>Outdoors </span>
+                    <span className={styles.headingPrimarySub}>is where life happens</span>
+                </h1>
+            </div>
         </header> 
     );
 };
