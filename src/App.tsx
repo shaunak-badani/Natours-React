73f6e87738d96f017d188f1a901bc645d0adee91
Shaunak Badani <shaunak.badani@duke.edu>
2026-06-13 01:42:20 +0530

    Built initial aspects of the card

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import FrontBanner from './pages/FrontBanner';
 import About from './pages/About'
 import "./pages/Feature.scss";
 import Feature from './pages/Feature';
+import CenterText from './components/CenterText';
+import { HeadingSecondary } from './components/Headings';
+import Row from './components/Row';
+import Col from './components/Col';
+import "./pages/Tours.scss";
+import "./components/Card.scss";
 
 function App() {
   return (
@@ -13,6 +19,27 @@ function App() {
         <main>
           <About />
           <Feature />
+          <section className="sectionTours">
+            <CenterText classNames={["uMarginBottomBig"]}>
+                <HeadingSecondary >
+                    Most popular tours
+                </HeadingSecondary>
+            </CenterText>
+            <Row>
+              <Col length={1} total={3}>
+                <div className="card">
+                  <div className="cardSide cardSideFront">
+                    Front
+                  </div>
+                  <div className="cardSide cardSideBack cardSideBack1">
+                    Back
+                  </div>
+                </div>
+              </Col>
+              <Col length={1} total={3}>Col 1 of 3</Col>
+              <Col length={1} total={3}>Col 1 of 3</Col>
+            </Row>
+          </section>
         </main>
       </section>
     </>
