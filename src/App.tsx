92333ef4795301fa5131f8429eaf4f07a11d8317
Shaunak Badani <shaunak.badani@duke.edu>
2026-06-13 02:20:59 +0530

    Completed styling the front part of the card

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,23 @@ function App() {
               <Col length={1} total={3}>
                 <div className="card">
                   <div className="cardSide cardSideFront">
-                    Front
+                    <div className="cardPicture cardPicture1">
+                      &nbsp;
+                    </div>
+                    <h4 className="cardHeading">
+                      <span className="cardHeadingSpan cardHeadingSpan1">
+                        The Sea Explorer
+                      </span>
+                    </h4>
+                    <div className="cardDetails">
+                      <ul>
+                        <li>3 day tours</li>
+                        <li>Up to 30 people</li>
+                        <li>2 tour guides</li>
+                        <li>Sleep in cozy hotels</li>
+                        <li>Difficulty: easy</li>
+                      </ul>
+                    </div>
                   </div>
                   <div className="cardSide cardSideBack cardSideBack1">
                     Back
